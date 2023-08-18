6bfc0f62be6cfeffa923f6b968e0505942835ed5
koshala <kosalamanojeewa@gmail.com>
2023-08-18 21:55:17 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<?php session_start(); ?>
+<html>
+	<head>
+		<!-- Meta Tags -->
+		<meta name="keywords" content="devyaka, phoneshop, vanillaPHP">
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="description" content="PhoneShop Admin Login Page">
+    	
+		<title>PhoneShop : Index</title>
+		
+		<!-- Start Styles -->
+			
+			<!-- Start Vendors -->
+			
+				<!-- Start Bootstrap -->
+				<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+				<link rel="stylesheet" href="dist/css/bootstrap-icons.css">
+				<!-- End Bootstrap -->
+				
+			<!-- End Vendors -->
+			
+			<!-- Start Common Styles -->
+			<link href="dist/css/common.css" rel="stylesheet">
+			<!-- End Common Styles -->
+			
+			<!-- Start Page Styles -->
+			<link href="dist/css/main.css" rel="stylesheet">
+            <script src="dist/js/core.js"></script>
+		<!-- End Page Styles -->
+		
+		<!-- End Styles -->
+	
+	</head>
+	<body>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col">
+						<div class="card">
+							<div class="card-body">
+								<h5 class="card-title text-uppercase text-center">Welcome</h5>
+								<p class="card-text"></p>
+<!--								<div class="mb-3">-->
+<!--									--><?php //
+//										if(isset($_SESSION['username'])){
+//											echo "You are logged in as: ".$_SESSION['username']." <a href='pages/dashboard/sales/'>continue session</a>";
+//										}
+//									?>
+<!--								</div>-->
+<!--								<div class="mb-3">-->
+<!--									<label for="emailInput" class="form-label text-muted">Email</label>-->
+<!--									<div class="input-group">-->
+<!--										<input type="email" class="form-control" id="emailInput" placeholder="name@example.com" aria-label="user email" aria-describedby="addonEmail" required>-->
+<!--										<span class="input-group-text visually-hidden addon" id="addonEmail">-->
+<!--										</span>-->
+<!--									</div>-->
+<!--									<div id="emailMsg"></div>-->
+<!--								</div>-->
+<!--								<div class="mb-3">-->
+<!--									<label for="passwordInput" class="form-label text-muted">Password</label>-->
+<!--									<div class="input-group">-->
+<!--										<input type="password" class="form-control" id="passwordInput" placeholder="password" aria-label="user password" aria-describedby="addonPwd" >-->
+<!--										<span class="input-group-text visually-hidden addon" id="addonPwd">-->
+<!--										</span>-->
+<!--									</div>-->
+<!--									<div id="pwdMsg"></div>-->
+<!--								</div>-->
+<!--								<div class="btn btn-primary w-100 text-uppercase log-btn" onclick="login();">-->
+<!--									<div class="spinner-border visually-hidden" role="status" id="logBtnSpinner">-->
+<!--										<span class="visually-hidden">Loading...</span>-->
+<!--									</div>-->
+<!--									<span id="logBtnText">Login</span>-->
+<!--								</div>-->
+<!--								<div id="primaryMsg"></div>-->
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+			
+		<!-- Start Scripts -->
+			
+			<!-- Start Vendors -->
+			
+				<!-- Start JQuery -->
+				<script src="dist/js/jquery-3.6.1.min.js"></script>
+				<!-- End JQuery -->
+				
+				<!-- Start Bootstrap -->
+				<script src="dist/js/bootstrap.min.js"></script>
+				<!-- End Bootstrap -->
+				
+			<!-- End Vendors -->
+			
+			<!-- Start Page Scripts -->
+
+			<!-- End Page Scripts -->
+		
+		<!-- End Scripts -->
+	</body>
+<html>	
